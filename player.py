991fbee2f7a61ea799615eefae1e9c3af94c887e
sarah-bruschi <sarah.bruschi@gmail.com>
2022-02-15 10:38:17 -0500

    create player class

--- player.py
+++ player.py
@@ -1,7 +1,15 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
 class Player:
-    pass
+   
+   def __init__(self) -> None:
+       super().__init__()
+       self.shape("turtle")
+       self.penup()
+       self.goto(STARTING_POSITION)
+       self.setheading(90)
