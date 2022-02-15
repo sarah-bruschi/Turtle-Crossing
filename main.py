df79f34e812b1276b1fa464ccfe37d15483e1784
sarah-bruschi <sarah.bruschi@gmail.com>
2022-02-15 10:41:23 -0500

    create go_up method

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ screen.tracer(0)
 
 player = Player()
 
+screen.listen()
+screen.onkey(player.go_up,"Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- player.py
+++ player.py
@@ -5,11 +5,14 @@ MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
+class Player(Turtle):
    
-   def __init__(self) -> None:
+    def __init__(self) -> None:
        super().__init__()
        self.shape("turtle")
        self.penup()
        self.goto(STARTING_POSITION)
        self.setheading(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
